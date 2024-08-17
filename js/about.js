ea815d369471b55dcd1d49b036af4cd2fa6d9fe6
Serhii Artamonov <sergeyartamonov9494@gmail.com>
2024-08-17 13:58:13 +0300

    1

---
+++ js/about.js
@@ -0,0 +1,9 @@
+/* Collapse */
+$("[data-collapse]").on("click", function (event) {
+  event.preventDefault();
+
+  var $this = $(this),
+    blockId = $this.data("collapse");
+
+  $this.toggleClass("active");
+});
